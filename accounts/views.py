e67d98aa814bf8db18b98c2ff2c647c18a7b6120
ervivekbhandari93 <ervivekbhandari@gmail.com>
2020-09-22 00:24:59 +0530

    account app create .

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import UserCreateForm 
+
+
+class SignUp(CreateView):
+    form_class = UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
