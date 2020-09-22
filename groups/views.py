bd5f7045186e2fcd399d09a45d30bc4d9b001a75
ervivekbhandari93 <ervivekbhandari@gmail.com>
2020-09-22 16:55:44 +0530

    worked with  views and templates files  that were in the  groups app.

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView, ListView
+
+
+class CreateGroup(LoginRequiredMixin, CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(DetailView):
+    model = Group
+
+
+class ListGroup(ListView):
+    model = Group
