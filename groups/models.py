ce445603eeee57928a8d8b681e950e48b1ab58b1
ervivekbhandari93 <ervivekbhandari@gmail.com>
2020-09-26 13:02:57 +0530

    bootstrap files(css, fonts, js) added to static/sns dir.

--- groups/models.py
+++ groups/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.utils.text import slugify
 from django.urls import reverse
 
+from django.utils.text import slugify
 import misaka
 
 from django.contrib.auth import get_user_model
